e3d44f6e4a7bc053fe2354820c740d9f02403c37
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-03 08:48:35 +0000

    main

---
+++ p7original.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction;
+int gcd(int a,int b)
+{
+  int t;
+  while (b != 0)
+    {
+      t=b;
+      b=a%b
+    }
+}
+int find_gcd(int a,int b);
+
