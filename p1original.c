152edcc48bef3da8a1c21d6e197c92a0722f6cb1
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-30 20:16:31 +0000

    assignment

--- p1original.c
+++ p1original.c
@@ -11,9 +11,9 @@ int gcd(int a,int b)
   int t;
     while (b!=0)
       {
-        t = b;
-        b = a%b;
-        a = t;
+        t =b;
+        b =a%b;
+        a =t;
       }
     return a;
 }
